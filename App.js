07c6a15d6400a40da749c85dbf2bc97315bd973a
Gustavo-1403 <134340500+Gustavo-1403@users.noreply.github.com>
2024-04-22 19:09:16 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,22 @@ import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
 
 import BottomTabNavigator from "./components/BottomTabNavigator";
+import LoginScreen from "./screens/Login"
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Inicial" component={BottomTabNavigator} />
+    </Stack.Navigator>
+  );
+}
 
 export default class App extends Component {
   constructor() {
@@ -26,7 +42,7 @@ export default class App extends Component {
   render() {
     const { fontLoaded } = this.state;
     if (fontLoaded) {
-      return <BottomTabNavigator />;
+      return <MyStack />;
     }
     return null;
   }
